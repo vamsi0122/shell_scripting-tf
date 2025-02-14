280078924838187083ef445cde91ec71c95fdf51
vamsi0122 <142490562+vamsi0122@users.noreply.github.com>
2025-02-14 14:37:19 +0530

    Update redis.sh

--- redis.sh
+++ redis.sh
@@ -28,11 +28,11 @@ VALIDATE(){
 }
 
 
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>$LOGFILE
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm --skip-broken -y &>>$LOGFILE
 
 VALIDATE $? "Installing Redis repo"
 
-yum module enable redis:remi-6.2 -y &>>$LOGFILE
+yum module enable redis:6 -y &>>$LOGFILE
 
 VALIDATE $? "Enabling Redis 6.2"
 
@@ -40,7 +40,7 @@ yum install redis -y &>>$LOGFILE
 
 VALIDATE $? "Installing Redis 6.2"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>$LOGFILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis.conf &>>$LOGFILE
 
 VALIDATE $? "Allowing Remote connections to redis"
 
